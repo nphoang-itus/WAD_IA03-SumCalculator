34a5c8fa7f01d34940b4ed1037455f1d0ba283ee
nphoang <nphuchoang.itus@gmail.com>
2025-11-03 17:36:12 +0700

    fix display wrong error

--- sum-calculator-app/src/components/SumCalculator.jsx
+++ sum-calculator-app/src/components/SumCalculator.jsx
@@ -27,8 +27,8 @@ function SumCalculator() {
     }
 
     // Validate if inputs use the correct decimal format (only '.' is allowed)
-    const decimalRegex = /^\d*\.?\d*$/; // Regex to match valid decimal numbers
-    if (!decimalRegex.test(number1) || !decimalRegex.test(number2)) {
+    const decimalRegex = /^\d*\,\d*$/; // Regex to match valid decimal numbers
+    if (decimalRegex.test(number1) || decimalRegex.test(number2)) {
       setError('\u26A0 Please use "." as the decimal separator!'); // Error for invalid decimal format
       return;
     }
